ab8f9b6ab39c1df6dd2b6843272f1447662edf79
irohitb <irohitbhatia@gmail.com>
2020-01-20 16:01:17 +0530

    feature: Added Changelog

---
+++ CHANGELOG.md
@@ -0,0 +1,28 @@
+# Version 1 (2020-01-20)
+
+## feature
+*  Added props for flatlist views and text ([e5eaab](https://github.com/jackyef/changelog-generator/commit/e5eaab04c811a314f9964330ec6a507c9d484a74))
+
+## feat
+*  fixed breaking changes ([d24df3](https://github.com/jackyef/changelog-generator/commit/d24df33b53b91300ea115a9a378edbc4b0fb5eb6))
+
+## chore
+*  package version fixed ([eb835a](https://github.com/jackyef/changelog-generator/commit/eb835a7403949ee3a932ff44ea12899ef62bc3e1))
+*  Readme Updated ([c24887](https://github.com/jackyef/changelog-generator/commit/c24887305a62fb821b862e07ba22389e067de5fb))
+*  Updated form progress ([31c771](https://github.com/jackyef/changelog-generator/commit/31c77136f8aeb21d846c2b00c382b149d9b7be80))
+*  Removed Package.lock ([a33887](https://github.com/jackyef/changelog-generator/commit/a338875e9ce627f2b5a716f0bf000ad0ccb19afe))
+
+## refactor
+*  Removed unecessary code and updated dependencies in package json ([4e8ba3](https://github.com/jackyef/changelog-generator/commit/4e8ba3d24a2c3e556de1aea21e6913780a80aa60))
+
+## bug
+*  Picker bottom  component removed from index.js and added missing styles ([e057cf](https://github.com/jackyef/changelog-generator/commit/e057cff34cae1cbf70e5697f7c63f8f738a01690))
+* Bug: forogot to declare props fix ([07fdcc](https://github.com/jackyef/changelog-generator/commit/07fdcc01ed3f6287568fd1afcd8efcd126c2e5a9))
+* Bug: File Reference in checkbox ([8b8ad7](https://github.com/jackyef/changelog-generator/commit/8b8ad79597296f82a23bbf57b6b5a0d8b600c805))
+
+## miscellaneous
+* Merge branch 'master' of https://github.com/irohitb/rn-formly ([c0eecc](https://github.com/jackyef/changelog-generator/commit/c0eeccc64468e58a9913e7763b8d936ac9c4ac46))
+*  Removed picker from bottom ([efaa2e](https://github.com/jackyef/changelog-generator/commit/efaa2e2e6118eb996deb63887d838f161beb2503))
+*  Auto Complete component styling changed to fix keyboard appearing over the box issue ([6f5fa1](https://github.com/jackyef/changelog-generator/commit/6f5fa1392b5b324a7fac9f1d973b1fd3482bc640))
+* initial commit ([8b6051](https://github.com/jackyef/changelog-generator/commit/8b605172a1ed2a2e724f5a2f10abdd53b50201ef))
+
---
+++ change-log-generator.js
@@ -0,0 +1,85 @@
+const child = require("child_process");
+const fs = require("fs");
+
+const output = child
+  .execSync(`git log --format=%B%H----DELIMITER----`)
+  .toString("utf-8");
+
+// Gives Commit with SHA 
+const commitsArray = output
+  .split("----DELIMITER----\n")
+  .map(commit => {
+    const [message, sha] = commit.split("\n");
+
+    return { sha, message };
+  })
+  .filter(commit => Boolean(commit.sha));
+
+
+
+const currentChangelog = fs.readFileSync("./CHANGELOG.md", "utf-8");
+// Gettting Package JSON version while Create logs 
+
+const currentVersion = parseFloat(require("./package.json").version);
+let newChangelog = `# Version ${currentVersion} (${
+  new Date().toISOString().split("T")[0]
+})\n\n`;
+
+
+const commits = {
+  feature: [], 
+  feat: [],
+  chore: [],
+  styles: [], 
+  refactor: [], 
+  breaking: [],  
+  bug: [], 
+  perf: [],
+  removed: [],
+  miscellaneous: []
+}
+
+
+commitsArray.forEach(commit => {
+const commitHeaderArray = commit.message.match(/.*(?=:)/)
+  if (commitHeaderArray) {
+    let commitHeader = commitHeaderArray[0].toLowerCase()
+    if (commits.hasOwnProperty(commitHeader)) {
+      commits[commitHeader].push(`* ${commit.message.replace(`${commitHeader}:`, "")} ([${commit.sha.substring(
+        0,
+        6
+      )}](https://github.com/jackyef/changelog-generator/commit/${
+        commit.sha
+      }))\n`)
+    } else {
+      commits.miscellaneous.push(`* ${commit.message.replace(`${commitHeader}:`, "")} ([${commit.sha.substring(
+        0,
+        6
+      )}](https://github.com/jackyef/changelog-generator/commit/${
+        commit.sha
+      }))\n`)
+    }
+  } else {
+    commits.miscellaneous.push(`* ${commit.message} ([${commit.sha.substring(
+      0,
+      6
+    )}](https://github.com/jackyef/changelog-generator/commit/${
+      commit.sha
+    }))\n`)
+  }
+})
+
+
+for (let commitHeader in commits) {
+  const commitsArray = commits[commitHeader]
+  if (commitsArray.length) {
+    newChangelog += `## ${commitHeader}\n`;
+    commitsArray.forEach(commitMessage => {
+      newChangelog += commitMessage;
+    })
+    newChangelog += '\n';
+  }
+}
+
+// prepend the newChangelog to the current one
+fs.writeFileSync("./CHANGELOG.md", `${newChangelog}${currentChangelog}`);
